2a21ab882eab84ba7b234145d256bc96e51ed013
zy <wuyingfengsui@gmail.com>
2015-02-15 18:05:32 +0800

    update example

--- examples/simple.js
+++ examples/simple.js
@@ -1,7 +1,28 @@
 var mate = require('../');
 
-var node = mate.createNode();
+var node = mate.createNode({
+	networkPort: 2017
+});
 
 node.on('listening', function() {
-	console.log('The mate node started.');
+	console.log('The mate node ' + node.localNodeContact.id.toString() + ' started');
+
+	node.directConnect({
+		ip: '127.0.0.1',
+		port: 2016
+	}, function(success) {
+		console.log('connect ' + success);
+		node.lookup('95499720-b4f6-11e4-afba-efd5b147fc02', function(success, lookupNode, fatherNode) {
+			if (success === true) {
+				if (fatherNode) {
+					console.log('find ' + lookupNode.id.toString() + ' from ' + fatherNode.id.toString());
+				} else {
+					console.log('find ' + lookupNode.id.toString() + ' locally');
+				}
+			} else {
+				console.log('can\'t find it');
+			}
+		});
+	});
+
 });
