532a1bf78f73a9fe9357dbac19518b175bd6f063
covertness <wuyingfengsui@gmail.com>
2015-03-05 22:51:01 +0800

    add root node

--- examples/simple.js
+++ examples/simple.js
@@ -1,33 +1,39 @@
 var mate = require('../');
 
 var node = mate.createNode({
-	networkPort: 2015
+	networkPort: 2015,
+	rootNode: {
+		ip: '203.195.153.192',
+		port: 2015
+	}
 });
 
 node.on('message', function(from, message) {
 	console.log('receive ' + from.id.toString() + ' message: ' + message);
-});
 
-node.on('listening', function() {
-	console.log('The mate node ' + node.localNodeContact.id.toString() + ' started');
-
-	node.directConnect({
-		ip: '203.195.153.192',
-		port: 2015
-	}, function(success, newNode) {
+	node.send(from.id.toString(), 'Hi!', function(success) {
 		if (success === true) {
-			console.log('direct connect node ' + newNode.id.toString() + ' success');
-
-			node.send('98875640-c0d9-11e4-a985-d3d0c6697805', 'I\'m here!', function(success) {
-				if (success === true) {
-					console.log('send message success');
-				} else {
-					console.log('send message failed');
-				}
-			});
+			console.log('send reply message success');
 		} else {
-			console.log('direct connect node failed');
+			console.log('send reply message failed');
 		}
 	});
+});
 
+node.on('root_connected', function() {
+	console.log('root node connected');
+
+/*
+	node.send('44e11b60-c1a7-11e4-9771-9180d4138974', 'I\'m here!', function(success) {
+		if (success === true) {
+			console.log('send message success');
+		} else {
+			console.log('send message failed');
+		}
+	});
+*/
+});
+
+node.on('root_disconnect', function() {
+	console.log('root node disconnect');
 });
